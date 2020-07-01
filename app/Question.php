cf9a8b6666cb0dcdd2403b0928273f9b9a764076
Chandra Ghimire <ghimire.chandra84@gmail.com>
2020-07-01 16:34:32 +0545

    question factory and seeder made to populate some data

--- app/Question.php
+++ app/Question.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Question extends Model
 {
@@ -13,6 +14,6 @@ class Question extends Model
     }
     public function setTitleAttribute($value){
         $this->attributes['title']=$value;
-        $this->attributes['slug']=str_slug($value,'-');
+        $this->attributes['slug']=Str::slug($value,'-');
     }
 }
