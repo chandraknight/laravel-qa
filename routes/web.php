79dfa1f6691e5ad845c3555a3567fa2c87c07a31
Chandra Ghimire <ghimire.chandra84@gmail.com>
2020-07-04 23:22:12 +0545

    Answer added to questions

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('questions','QuestionsController')->except('show');
 Route::get('questions/{slug}','QuestionsController@show')->name('questions.show');
+/*Both are same*/
+//Route::post('/questions/{question}/answers','AnswersController@store')->name('answers.store');
+Route::resource('questions.answers','AnswersController')->only(['store','edit','update','destory']);
+/*******/
