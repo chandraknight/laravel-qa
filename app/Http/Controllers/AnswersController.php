f3ea924eb423da575b2823027966c77d5ca1e49f
Chandra Ghimire <ghimire.chandra84@gmail.com>
2020-07-13 18:16:13 +0545

    edit and update answer is implemented invue js also user v-cloak to load form

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -12,7 +12,7 @@ class AnswersController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Question $question, Request $request)
@@ -27,7 +27,7 @@ class AnswersController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Answer  $answer
+     * @param \App\Answer $answer
      * @return \Illuminate\Http\Response
      */
     public function edit(Question $question, Answer $answer)
@@ -40,8 +40,8 @@ class AnswersController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Answer  $answer
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Answer $answer
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Question $question, Answer $answer)
@@ -51,6 +51,12 @@ class AnswersController extends Controller
         $answer->update($request->validate([
             'body' => 'required',
         ]));
+        if ($request->expectsJson()) {
+            return response()->json([
+                'message' => 'Your answer has been updated',
+                'body_html'=>$answer->body_html
+            ]);
+        }
 
         return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated');
     }
@@ -58,7 +64,7 @@ class AnswersController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Answer  $answer
+     * @param \App\Answer $answer
      * @return \Illuminate\Http\Response
      */
     public function destroy(Question $question, Answer $answer)
