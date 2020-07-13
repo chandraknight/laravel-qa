5f0fdb596043c534c31d825d0b163e50b1b7b034
Chandra Ghimire <ghimire.chandra84@gmail.com>
2020-07-13 22:08:19 +0545

    Ajaxify the delete answer .

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -8,7 +8,8 @@
                 bodyHtml: this.answer.body_html,
                 id: this.answer.id,
                 questionId: this.answer.question_id,
-                beforeEditCache: null
+                beforeEditCache: null,
+
             }
         },
         methods: {
@@ -22,22 +23,34 @@
             },
 
             update() {
-                axios.patch(`/questions/${this.questionId}/answers/${this.id}`, {
+                axios.patch(this.endpoint, {
                     body: this.body
-                })
-                    .then(res => {
+                }).then(res => {
                         this.editing = false;
                         this.bodyHtml = res.data.body_html;
                         alert(res.data.message);
-                    })
-                    .catch(err => {
+                    }).catch(err => {
                         alert(err.response.data.message);
                     });
+            },
+            destory(){
+                if(confirm('Are you sure?')){
+                  axios.delete(this.endpoint).then(res=>{
+                      $(this.$el).fadeOut(500,()=>{
+                          alert(res.data.message);
+                      })
+                  }).catch(err=>{
+                      alert(err.response.data.method)
+                  });
+                }
             }
         },
         computed: {
             isInvalid() {
                 return this.body.length < 10;
+            },
+            endpoint(){
+                return `/questions/${this.questionId}/answers/${this.id}`;
             }
         }
 
